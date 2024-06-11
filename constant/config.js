e546d543a9dbdafe7158fc8da0ce864c509113b4
mohammad6vakili <mohammad6vakili@gmail.com>
2024-06-11 10:22:33 +0330

    fix(change address): :bug: change address in config

--- constant/config.js
+++ constant/config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
   EMAIL_ADDRESS: "baharpodology@gmail.com",
   WHATSAPP_NUMBER: "https://api.whatsapp.com/send?phone=09361816840",
   INSTAGRAM_ID: "https://instagram.com/baharspodology",
-  ADRESS: "آدرس : قزوین، دهخدا، خ فلسطین، خ فردوسی شمالی، خ مهجور",
-  ADRESS_LAT: 36.2812,
-  ADDRESS_LNG: 50.0079,
+  ADRESS: "آدرس : قزوین، چهارراه شهرداری، بالای دندان پزشکی شفا، طبقه ۴",
+  ADRESS_LAT: 36.2711,
+  ADDRESS_LNG: 50.0081,
 };
